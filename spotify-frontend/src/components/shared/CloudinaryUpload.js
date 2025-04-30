b80686686cfcced5c3f41673fa95a5165d58ebb1
Your Name <youremail@example.com>
2025-04-30 21:00:14 +0530

    Initial commit - My Spotify Clone

---
+++ spotify-frontend/src/components/shared/CloudinaryUpload.js
@@ -0,0 +1,37 @@
+import {openUploadWidget} from "../../utils/CloudinaryService";
+import config from "../../config";
+
+
+const CloudinaryUpload = ({setUrl, setName}) => {
+    const uploadImageWidget = () => {
+        let myUploadWidget = openUploadWidget(
+            {
+                cloudName: "dlmaf2wqn",
+                uploadPreset: config.cloudinaryuploadpreset,
+                sources: ["local"],
+            },
+            function (error, result) {
+                if (!error && result.event === "success") {
+                    setUrl(result.info.secure_url);
+                    setName(result.info.original_filename);
+                } else {
+                    if (error) {
+                        console.log(error);
+                    }
+                }
+            }
+        );
+        myUploadWidget.open();
+    };
+
+    return (
+        <button
+            className="bg-white text-black  rounded-full p-4 font-semibold"
+            onClick={uploadImageWidget}
+        >
+            Select Track
+        </button>
+    );
+};
+
+export default CloudinaryUpload;
